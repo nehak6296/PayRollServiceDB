051259c1738186eb77871657342ee2342bf33814
Neha <nehak6296@gmail.com>
2021-04-09 11:53:27 -0700

    [Neha]Added:Updating gender of employees

---
+++ UpdateGender.sql
@@ -0,0 +1,8 @@
+
+alter table employee_payroll add gender char(1)
+
+
+update employee_payroll set gender='M' where name = 'Charlie'
+update employee_payroll set gender='M' where name = 'Billi'
+update employee_payroll set gender='F' where name = 'Terisa'
+
