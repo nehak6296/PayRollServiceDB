585ebed0e646915bb59c46f06b30a30e98626942
Neha <nehak6296@gmail.com>
2021-04-14 15:35:14 +0530

    [Neha]Added:Added columns Deduction,IncomeTax,TaxablePay,NetPayand renamed salary to basic_pay

---
+++ AddDeductionIncomeTaxNetPayTaxablePay.sql
@@ -0,0 +1,4 @@
+alter table employee_payroll add  deductions money ;
+alter table employee_payroll add  Taxable_pay money;
+alter table employee_payroll add  Income_Tax money ;
+alter table employee_payroll add  Net_pay money ;
