7fc8762980164e2ce194dfb2f7b664db0057f4fe
Neha <nehak6296@gmail.com>
2021-04-14 17:19:59 +0530

    [Neha]Added: Terisa is added to 2 departments as in Sales and Marketing

---
+++ AddEmployeeTo2Departments.sql
@@ -0,0 +1,8 @@
+
+insert into employee_payroll
+(name, basic_pay, start, gender, phone_number,department, Deduction, Taxable_pay, Incometax, Netpay) values
+('Terisa', 150000.00, '2019-12-03', 'F','8884443332', 'Marketing', 20000.00, 10000.00, 200000.00, 300000.00);
+
+insert into employee_payroll
+(name, basic_pay, start, gender, phone_number,department) values
+('Terisa', 150000.00, '2019-12-03', 'F','8884443332', 'Sales');
